b8358e90a9c7ea4a8ef70c3436374d947235ef56
Disha-Parikh <dishaparikh98@gmail.com>
2019-10-11 11:10:01 +0530

    trying to trigger webhook

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html', the_title='TIGER HOME PAGE!!')
+    return render_template('index.html', the_title='TIGER HOME PAGE!! please trigger  ')
 
 @app.route('/symbol.html')
 def symbol():
